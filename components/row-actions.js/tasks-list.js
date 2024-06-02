4811de10e18468cedbd29fbc673bb8b1b7d215d6
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2024-06-02 05:18:38 +0200

    task list

--- components/row-actions.js/tasks-list.js
+++ components/row-actions.js/tasks-list.js
@@ -17,8 +17,25 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form"
+import { useForm } from "react-hook-form"
 
 import { z } from "zod"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog"
 
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
@@ -30,12 +47,59 @@ export const taskSchema = z.object({
 })
 
 import { labels } from "@/constants/list"
+import { useState } from "react"
+import { deleteTask } from "@/db/collections/task"
+import { Loader2 } from "lucide-react"
+import { toast } from "sonner"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Input } from "../ui/input"
 
 export function TasksListRowActions({ row }) {
   const task = taskSchema.parse(row.original)
+ 
+
+  const onSubmit = async (values) => {
+    const { id } = data
+   
+    console.log(values,"valuesUserId")
+    try {
+      setIsLoading(true)
+      await updateProject(id, values)
+      toast("Project updated.")
+    setShowUpdateDialog(false)
+    setIsLoading(false)
+    triggerRefetch()
+    } catch (error) {
+      toast.error("Project updated.")
+
+    }
+    finally{
+      setIsLoading(false)
+    }
+    
+  }
+  
+
+  const form = useForm({
+    resolver: zodResolver(taskSchema),
+    defaultValues: {
+      name: "",
+      columnId: "",
+      priority: ""
+    },
+  })
+
+
+
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false)
+  const [showUpdateDialog,setShowUpdateDialog] = useState(false)
+
+  const [isLoading,setIsLoading] = useState()
+
 
   return (
-    <DropdownMenu>
+    <>
+          <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant="ghost"
@@ -46,28 +110,109 @@ export function TasksListRowActions({ row }) {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem>Edit</DropdownMenuItem>
-        <DropdownMenuItem>Make a copy</DropdownMenuItem>
-        <DropdownMenuItem>Favorite</DropdownMenuItem>
+      <DropdownMenuItem onClick={() => setShowUpdateDialog(true)}>
+          Edit
+          <DropdownMenuShortcut>⌘</DropdownMenuShortcut>
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuSub>
-          <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
-          <DropdownMenuSubContent>
-            <DropdownMenuRadioGroup value={task.label}>
-              {labels.map((label) => (
-                <DropdownMenuRadioItem key={label.value} value={label.value}>
-                  {label.label}
-                </DropdownMenuRadioItem>
-              ))}
-            </DropdownMenuRadioGroup>
-          </DropdownMenuSubContent>
-        </DropdownMenuSub>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
           Delete
           <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
+
+    <Dialog open={showDeleteDialog} onOpenChange={setShowDeleteDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              project and remove its data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="ghost" onClick={() => setShowDeleteDialog(false)}>
+              Cancel
+            </Button>
+            <Button type="submit" disabled={isLoading}
+              variant="destructive"
+              onClick={async () => {
+                setIsLoading(true)
+                
+                await deleteTask(row?.original?.id)
+                setShowDeleteDialog(false)
+                setIsLoading(false)
+                toast("Task deleted.")
+                 
+              }}
+            >
+                  {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              Delete project
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
+
+      <Dialog open={showUpdateDialog} onOpenChange={setShowUpdateDialog}>
+        <DialogContent className="sm:max-w-[425px]">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <DialogHeader>
+                <DialogTitle>Update Project</DialogTitle>
+                <DialogDescription>
+                  Fill in the form below to update the project.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="grid grid-cols-4 items-center gap-4">
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Project X"
+                          className="col-span-3"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="col-span-4" />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem className="grid grid-cols-4 items-center gap-4">
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Marketing campaign"
+                          className="col-span-3"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="col-span-4" />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter >
+              <Button type="submit" disabled={isLoading}>
+              {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                Save changes
+                
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
